05d1d134deaeb41011e84ff8c46a64184fafa173
Hunter Alexander <hunter.rm.alexander@gmail.com>
2018-08-25 14:41:58 -0700

    linter fixes

--- queue.py
+++ queue.py
@@ -1,23 +1,27 @@
+"""Create Queue class"""
 class Queue:
-    
+    """Creates Queue Class"""
+
     def __init__(self):
         self.store = {}
         self.min = 0
         self.max = 0
-        
+
     def enqueue(self, value):
+        """adds to queue"""
         self.store[self.max] = value
-        self.max +=1
-        
+        self.max += 1
+
     def dequeue(self):
+        """returns first value in queue and removes it from queue"""
         oldest = self.store[self.min]
         del self.store[self.min]
-        self.min +=1
+        self.min += 1
         return oldest
 
-x = Queue()
-x.enqueue(10)
-print(x.store)
-x.enqueue(5)
-print(x.store)
-print(x.store, x.dequeue())
+X = Queue()
+X.enqueue(10)
+print(X.store)
+X.enqueue(5)
+print(X.store)
+print(X.store, X.dequeue())
