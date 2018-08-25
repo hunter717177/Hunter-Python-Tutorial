c8a88cccfa65e58d15f252a92a4673a574943a07
Hunter Alexander <hunter.rm.alexander@gmail.com>
2018-08-25 13:45:28 -0700

    adds stack class

--- stack.py
+++ stack.py
@@ -1,1 +1,21 @@
-print('HELLO')
+class Stack:
+
+    def __init__(self):
+        self.store = {}
+        self.length = 0
+
+    def push(self, value):   
+        self.length += 1
+        self.store[self.length] = value
+
+    def pop(self):
+        latest = self.store[self.length]
+        del self.store[self.length]
+        self.length -= 1
+        return latest
+
+x = Stack()
+x.push(10)
+x.push(5
+print(x.store)
+print(x.store, x.pop())
